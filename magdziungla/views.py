fd8d0d68477848740ad691d7e53e9beb19d5efde
Rafal <darklighter79@gmail.com>
2022-06-05 22:44:25 +0200

    updated templates

--- magdziungla/views.py
+++ magdziungla/views.py
@@ -3,17 +3,13 @@ from django.shortcuts import render
 from .forms import PlantForm, PotForm, PlanForm, SupplierForm, LocationForm, SoilForm
 from .models import Plant, Plan
 
+
 # Create your views here.
-
-
 
 
 def page(request):
     """main page view"""
     return render(request, 'main_page.html')
-
-
-
 
 
 def all_plants(request):
@@ -22,15 +18,10 @@ def all_plants(request):
     return render(request, 'plant_list.html', {'plants': plants})
 
 
-
-
-
 def all_plans(request):
     """view of all plans currently uploaded to the databse"""
     plans = Plan.objects.all()
     return render(request, 'plan_list.html', {'plans': plans})
-
-
 
 
 def add_plant(request):
